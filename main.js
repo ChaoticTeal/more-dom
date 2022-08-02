abd249af72b7d32e3675ac51b55f0273728c3389
Andy Mina <mina.andy54@gmail.com>
2022-08-01 20:58:23 -0400

    feat: finish starter code

---
+++ main.js
@@ -0,0 +1,41 @@
+/**
+ * @TODO implement the onclick event listeners for starters
+ * which should:
+ *      - change the innerHTML of #poke-text to their id
+ */
+
+/**
+ * @TODO implement addPika button which onclick should:
+ *      - create a new img
+ *      - set the src images/pikachu.png
+ *      - set the id to Pikachu
+ *      - add "poke-img" to the classes
+ *      - add any necessary event listeners
+ * 
+ * and then append it to #img-container
+ */
+
+/**
+ * @TODO implement the rmvPika button which onclick should:
+ *      - remove the pikachu image
+ */
+
+/**
+ * @TODO use a for-loop to prgrammatically add all necessary
+ * event listeners
+ */
+
+/**
+ * @TODO EXTRA CREDIT implement the onmouseover event listener
+ * for all starters which should:
+ *      - give the current starter a red border
+ *      - add a black box shadow of 5px x-offset, 5px y-offset,
+ *        and a blur radius of 5px
+ */
+
+/**
+ * @TODO EXTRA CREDIT implement the onmouseout event listener
+ * for all starters which should:
+ *      - remove the box shadow
+ *      - remove the red border
+ */
